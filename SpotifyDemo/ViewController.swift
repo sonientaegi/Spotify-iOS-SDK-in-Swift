6cca2a6501c10a3ff2c47ae922e1ff579eecc013
손영택 <sonientaegi@gmail.com>
2016-05-20 14:56:13 +0900

    Initial Commit

---
+++ SpotifyDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotifyDemo
+//
+//  Created by 손영택 on 2016. 5. 20..
+//  Copyright © 2016년 손영택. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
