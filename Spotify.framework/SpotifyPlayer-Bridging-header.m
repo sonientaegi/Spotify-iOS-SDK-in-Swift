83065dbeec05f0f1d047f458b5e7762e222bddfa
손영택 <sonientaegi@gmail.com>
2016-05-20 15:07:13 +0900

    1st commit

---
+++ Spotify.framework/SpotifyPlayer-Bridging-header.m
@@ -0,0 +1,9 @@
+//
+//  SpotifyPlayer-Bridging-header.m
+//  SpotifyPlayer
+//
+//  Created by 손영택 on 2016. 5. 17..
+//  Copyright © 2016년 SonienTaegi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
--- SpotifyDemo/ViewController.swift
+++ SpotifyDemo/ViewController.swift
@@ -2,14 +2,18 @@
 //  ViewController.swift
 //  SpotifyDemo
 //
-//  Created by 손영택 on 2016. 5. 20..
-//  Copyright © 2016년 손영택. All rights reserved.
+//  Created by SonienTaegi on 2016. 5. 18..
 //
+//  Contact    sonientaegi@gmail.com
+//             sonien.net
+//
+//  Copyright © 2016년 Sonien Studio. All rights reserved.
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SPTAuthViewDelegate {
+    var player:SPTAudioStreamingController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,83 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewDidAppear(animated: Bool) {
+        let auth = SPTAuth.defaultInstance()
+        auth.clientID        = "Put your own value"
+        auth.redirectURL     = NSURL.init(string:"Put your own url")
+        auth.requestedScopes = [SPTAuthStreamingScope]
+        
+        let authvc = SPTAuthViewController.authenticationViewController()
+        // authvc.clearCookies(nil)
+        authvc.modalPresentationStyle   = UIModalPresentationStyle.OverCurrentContext
+        authvc.modalTransitionStyle     = UIModalTransitionStyle.CrossDissolve
+        authvc.delegate                 = self
+        
+        self.modalPresentationStyle     = UIModalPresentationStyle.CurrentContext
+        self.definesPresentationContext = true
+        self.presentViewController(authvc, animated: true, completion: nil)
+    }
+    
+    func authenticationViewController(authenticationViewController: SPTAuthViewController!, didLoginWithSession session: SPTSession!) {
+        print("Login")
+        initiatePlayer(session)
+    }
+    
+    func authenticationViewController(authenticationViewController: SPTAuthViewController!, didFailToLogin error: NSError!) {
+        print("Fail to login")
+    }
+    
+    func authenticationViewControllerDidCancelLogin(authenticationViewController: SPTAuthViewController!) {
+        print("Cancel login")
+    }
+    
+    func initiatePlayer(session: SPTSession!) -> Bool {
+        if self.player == nil {
+            self.player = SPTAudioStreamingController.init(clientId: SPTAuth.defaultInstance().clientID)
+        }
+        
+        if let player = self.player {
+            if !player.loggedIn {
+                player.loginWithSession(session, callback: self.didLogin)
+            }
+            else {
+                performSelector(#selector(self.didLogin), withObject: nil)
+            }
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+    @objc private func didLogin(error:NSError!) {
+        // print(NSThread.isMainThread())
+        
+        if error != nil {
+            let msg = "FAIL : " + error.debugDescription
+            print(msg)
+        }
+        else if let player = self.player {
+            let trackUri:NSURL! = NSURL.init(string: "spotify:track:58s6EuEYJdlb0kO7awm3Vp")
+            player.playURIs([trackUri], fromIndex: 0, callback: self.didPlay)
+            print("Playing request")
+        }
+        else {
+            print("Player is not initiated")
+        }
+    }
+    
+    @objc private func didPlay(error:NSError!) {
+        // print(NSThread.isMainThread())
+        
+        if error != nil {
+            let msg = "FAIL : " + error.debugDescription
+            print(msg)
+        }
+        else {
+            print("Now playing")
+        }
+    }
 }
 
